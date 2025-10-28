3230be6132e385605047f8d6cfcf916a45911d2d
coders <example@mail>
2025-10-28 10:17:37 -0500

    add the seeder and finalize the first hu

--- src/dragons/entities/dragon.entity.ts
+++ src/dragons/entities/dragon.entity.ts
@@ -5,12 +5,22 @@ import { Adoption } from "../../adoptions/entities/adoption.entity";
 
 @Entity("dragons")
 export class Dragon extends BaseEntity {
+  @Column()
   name: string;
-  @Column({ type: "enum", enum: DragonTypes }) type: DragonTypes;
-  @Column({ type: "enum", enum: DragonStatus, default: DragonStatus.AVAILABLE })
+
+  @Column({ type: "enum", enum: DragonTypes })
+  type: DragonTypes;
+
+  @Column({
+    type: "enum",
+    enum: DragonStatus,
+    default: DragonStatus.AVAILABLE,
+  })
   status: DragonStatus;
+
   @Column()
   age: number;
+
   @OneToMany(() => Adoption, (adoption) => adoption.dragon)
   adoptions: Adoption[];
 }
---
+++ src/seeder/data/users.ts
@@ -0,0 +1,14 @@
+export const usersData = [
+  {
+    name: 'Master Admin',
+    email: 'master@gmail.com',
+    password: '123456',
+    role: 'admin',
+  },
+  {
+    name: 'Ash ketchup',
+    email: 'ash@gmail.com',
+    password: '123456',
+    role: 'caretaker',
+  }
+]
